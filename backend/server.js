70d507c5554aa5c3f5842b68dd9167b3392e56de
lingyunxiao <xiao.ling@northeastern.edu>
2024-03-10 18:13:59 -0700

    mar10 18:13

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const {
   notFound,
   errorHandler,
 } = require("../backend/middleware/errorMiddleware");
 const chatRoutes = require("./routes/chatRoutes");
-
+const { sendMessage } = require("./models/messageModel");
 const app = express();
 app.use(express.json()); // to access the json data
 dotenv.config();
@@ -21,7 +22,7 @@ connectDB();
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
-
+app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
